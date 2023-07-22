0b781e41d1ff83742aa65b2646488b749775cb08
Oluwagbenga Ogundare <oluwagbengaogundare@gmail.com>
2023-07-21 21:45:56 -0400

    moved lightbnb web app from a different repo

--- 1_queries/05_all_my_reservations.sql
+++ 1_queries/05_all_my_reservations.sql
@@ -1,6 +1,10 @@
-SELECT res.id, property, PP.title, res.start_date, pp.cost_per_night, AVG(rating)
+SELECT res.id, pp.title, pp.cost_per_night,res.start_date, AVG(pr.rating) as Average_Rating
 FROM reservations res 
 LEFT JOIN properties pp
-res.property_id  = pp.id
+ON res.property_id  = pp.id
 JOIN property_reviews pr 
-pr.property_id = res.property_id
+ON pr.property_id = res.property_id
+WHERE res.guest_id = 1
+GROUP BY res.id, pp.title, res.start_date, pp.cost_per_night
+ORDER BY res.start_date 
+LIMIT 10;
